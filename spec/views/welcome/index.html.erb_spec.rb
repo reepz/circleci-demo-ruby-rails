fef2b38b541719dcc3a497031a3a0119ec514b4e
reepz <a.wojtowicz@outlook.com>
2019-11-18 22:09:32 +0100

    test fixed

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "welcome/index.html.erb", type: :view do
   it 'renders h1 tag with Viara text' do
     render
-    assert_select "h1>", :text => "Viara".to_s, :count => 1
+    assert_select "h1", "Viara"
   end
 end
